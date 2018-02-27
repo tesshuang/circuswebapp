504f54373625e2c3aa33e9f123b869d0acddeec8
Tess <tesshuangxj@yahoo.com>
2018-02-27 15:58:28 -0800

    add css for conplay interface

--- src/comp/playground/contest/ConPlay.js
+++ src/comp/playground/contest/ConPlay.js
@@ -13,10 +13,21 @@ class ConPlay extends Component {
         
         return (
           <div>
-            LET US PLAY
-            {this.props.playname}
-            <img src={this.props.playbank[this.props.playava]} height={50} alt="player"/>
-            <button onClick={this.props.leaveContest.bind(this,0)}>Leave the Room</button>
+            <div className="cont_left">
+                <h2>Player1</h2>
+                <p>{this.props.playname}</p>
+            
+                <img src={this.props.playbank[this.props.playava]} height={50} alt="player"/>
+                <button onClick={this.props.leaveContest.bind(this,0)}>Leave the Room</button>
+            </div>
+            <div className="cont_center">
+                <h2>LET US PLAY</h2>
+            </div>
+            <div className="cont_right">
+                <h2>Player2</h2>
+                <p>name</p>
+                <p>IMAGE</p>
+            </div>
           </div>
         );
     }
