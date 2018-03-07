ea1a09a8a57f2f5e6fc69f62616005d8917dff3c
Tess <tesshuangxj@yahoo.com>
2018-03-06 17:41:54 -0800

    push question variables

--- src/comp/playground/contest/ConEntry.js
+++ src/comp/playground/contest/ConEntry.js
@@ -16,7 +16,7 @@ class ConEntry extends Component {
             <h4>Pick a genre</h4>
             <button className="join_btn" onClick={this.props.joinCon.bind(this,"general")}>GENERAL</button><br/>
             <button className="join_btn" onClick={this.props.joinCon.bind(this,"d3")}>D3</button><br/>
-            <button className="join_btn" onClick={this.props.joinCon.bind(this,"gym")}>RECREATION</button><br/>
+            <button className="join_btn" onClick={this.props.joinCon.bind(this,"recreation")}>RECREATION</button><br/>
             <button className="join_btn" onClick={this.props.joinCon.bind(this,"library")}>LIBRARY</button>
             
           </div>
--- src/comp/playground/contest/ConPlay.js
+++ src/comp/playground/contest/ConPlay.js
@@ -4,13 +4,67 @@ class ConPlay extends Component {
     constructor(props){
         super(props);
         this.state = {
-            
+            allquestion:[],
+            myindex:0
         }
+        
+        this.startGame = this.startGame.bind(this);
+        this.changeQuestions = this.changeQuestions.bind(this);
         
     }
 
+    startGame(){
+        fetch('/getquiz/'+this.props.roomstring,{
+            method:"GET",
+            headers: new Headers({
+                'Content-Type': 'application/json'
+              })
+        }).then((resp)=>{
+            return resp.json();
+        })
+          .then((data)=>{
+            console.log(data);
+            this.setState({
+              allquestion:data,
+            })
+        });
+        this.props.dismissHead();
+    }
+    
+    changeQuestions(){
+        this.setState({
+            myindex:this.state.myindex+1
+        })
+    }
+    
     render() {
         var comp = null;
+        var allqs = null;
+        var startgame = null;
+        
+        if(this.props.showgame === true){
+            startgame =(
+                <div>
+                    <h2>LET US PLAY</h2>
+                    <button onClick={this.startGame}>START THE GAME</button>
+                </div>
+            );
+        }
+        
+        if(this.state.allquestion.length !== 0){
+            allqs = (
+                <div>
+                <h1>Test yourself</h1> 
+                    <h3>{this.state.allquestion[this.state.myindex].question}</h3>
+                    <input ref="ans1" type="radio" name="gender" value={this.state.allquestion[this.state.myindex].ans1}/>{this.state.allquestion[this.state.myindex].ans1}<br/>
+                    <input type="radio" name="gender" value={this.state.allquestion[this.state.myindex].ans2} />{this.state.allquestion[this.state.myindex].ans2}<br/>
+                    <input type="radio" name="gender" value={this.state.allquestion[this.state.myindex].ans3} />{this.state.allquestion[this.state.myindex].ans3}<br/>
+                    <input type="radio" name="gender" value={this.state.allquestion[this.state.myindex].ans4} />{this.state.allquestion[this.state.myindex].ans4}<br/> 
+                    <button onClick={this.changeQuestions}>Submit</button>
+                </div>
+            );
+        }
+        
         if(this.props.conusers.length >= 2){
             comp= (
                 <div>
@@ -30,7 +84,10 @@ class ConPlay extends Component {
                 <button className="leave_btn" onClick={this.props.leaveContest.bind(this,0)}>Leave the Room</button>
             </div>
             <div className="cont_center">
-                <h2>LET US PLAY</h2>
+                <div className="cont_upper">
+                    {startgame}
+                </div>
+                {allqs}
             </div>
             <div className="cont_right">
                 {comp}
