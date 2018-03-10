17ef9f4871bce285f8d11e1312121d3de8894865
Sherry Chen <a00942632@se14l0113-00.ad.bcit.ca>
2018-03-09 17:53:57 -0800

    connection file

---
+++ src/comp/playground/Connect.js
@@ -0,0 +1,10 @@
+export default{
+    local1:"http://localhost:4005",
+    dev1:"https://circussocket.herokuapp.com/",
+    local2:"http://localhost:4006",
+    dev2:"https://midautumnfestivalcard.herokuapp.com/",
+    local3: "http://localhost:4008",
+    dev3:"https://pizsocket.herokuapp.com/",
+    local4:"http://localhost:4007",
+    dev4:"https://contsocket.herokuapp.com/"
+}
